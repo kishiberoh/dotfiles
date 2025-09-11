234fe336ac83d82aff90aa1386e741aacc1e504e
Mamoun <mamoun.imghi@epfl.ch>
2025-09-12 01:08:29 +0200

    nvim wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -163,15 +163,15 @@ require("lazy").setup({
 				require("mini.icons").setup()
 			end,
 		},
-		{
-			"echasnovski/mini.files",
-			version = false,
-			config = function()
-				require("mini.files").setup({
-					vim.keymap.set("n", "<leader>e", ":lua MiniFiles.open()<CR>"),
-				})
-			end,
-		},
+		-- {
+		-- 	"echasnovski/mini.files",
+		-- 	version = false,
+		-- 	config = function()
+		-- 		require("mini.files").setup({
+		-- 			-- vim.keymap.set("n", "<leader>ee", ":lua MiniFiles.open()<CR>"),
+		-- 		})
+		-- 	end,
+		-- },
 		{
 			"windwp/nvim-autopairs",
 			event = "InsertEnter", -- Only load when you enter Insert mode
@@ -222,8 +222,28 @@ require("lazy").setup({
 			"nvim-lualine/lualine.nvim",
 			lazy = true,
 			event = { "BufReadPre", "BufNewFile" },
+			opts = function(_, opts)
+				opts = opts or {}
+				opts.sections = opts.sections or {}
+				opts.sections.lualine_c = opts.sections.lualine_c or {}
+				local trouble = require("trouble")
+				local symbols = trouble.statusline({
+					mode = "lsp_document_symbols",
+					groups = {},
+					title = false,
+					filter = { range = true },
+					format = "{kind_icon}{symbol.name:Normal}",
+					-- The following line is needed to fix the background color
+					-- Set it to the lualine section you want to use
+					hl_group = "lualine_c_normal",
+				})
+				table.insert(opts.sections.lualine_c, {
+					symbols.get,
+					cond = symbols.has,
+				})
+			end,
 			config = function()
-				require("lualine").setup()
+				require("lualine").setup({})
 			end,
 		},
 		{
@@ -243,56 +263,56 @@ require("lazy").setup({
 				{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 			},
 		},
-		-- {
-		-- 	"lewis6991/gitsigns.nvim",
-		-- 	signs = {
-		-- 		add = { text = "┃" },
-		-- 		change = { text = "┃" },
-		-- 		delete = { text = "_" },
-		-- 		topdelete = { text = "‾" },
-		-- 		changedelete = { text = "~" },
-		-- 		untracked = { text = "┆" },
-		-- 	},
-		-- 	signs_staged = {
-		-- 		add = { text = "┃" },
-		-- 		change = { text = "┃" },
-		-- 		delete = { text = "_" },
-		-- 		topdelete = { text = "‾" },
-		-- 		changedelete = { text = "~" },
-		-- 		untracked = { text = "┆" },
-		-- 	},
-		-- 	signs_staged_enable = true,
-		-- 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-		-- 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-		-- 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-		-- 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-		-- 	watch_gitdir = {
-		-- 		follow_files = true,
-		-- 	},
-		-- 	auto_attach = true,
-		-- 	attach_to_untracked = false,
-		-- 	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-		-- 	current_line_blame_opts = {
-		-- 		virt_text = true,
-		-- 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-		-- 		delay = 1000,
-		-- 		ignore_whitespace = false,
-		-- 		virt_text_priority = 100,
-		-- 		use_focus = true,
-		-- 	},
-		-- 	current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
-		-- 	sign_priority = 6,
-		-- 	update_debounce = 100,
-		-- 	status_formatter = nil, -- Use default
-		-- 	max_file_length = 40000, -- Disable if file is longer than this (in lines)
-		-- 	preview_config = {
-		-- 		-- Options passed to nvim_open_win
-		-- 		style = "minimal",
-		-- 		relative = "cursor",
-		-- 		row = 0,
-		-- 		col = 1,
-		-- 	},
-		-- },
+		{
+			"lewis6991/gitsigns.nvim",
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged_enable = true,
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			attach_to_untracked = false,
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+		},
 		{
 			"stevearc/conform.nvim",
 			event = { "BufReadPre", "BufNewFile" },
@@ -422,7 +442,7 @@ require("lazy").setup({
 				},
 			},
 			event = "InsertEnter", -- optional: provides snippets for the snippet source
-			dependencies = { "rafamadriz/friendly-snippets" },
+			-- dependencies = { "rafamadriz/friendly-snippets" },
 
 			-- use a release tag to download pre-built binaries
 			version = "1.*",
@@ -982,6 +1002,43 @@ require("lazy").setup({
 			---@type render.md.UserConfig
 			opts = {},
 		},
+		{
+			"folke/trouble.nvim",
+			opts = {}, -- for default options, refer to the configuration section for custom setup.
+			cmd = "Trouble",
+			keys = {
+				{
+					"<leader>xx",
+					"<cmd>Trouble diagnostics toggle<cr>",
+					desc = "Diagnostics (Trouble)",
+				},
+				{
+					"<leader>xX",
+					"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+					desc = "Buffer Diagnostics (Trouble)",
+				},
+				{
+					"<leader>cs",
+					"<cmd>Trouble symbols toggle focus=false<cr>",
+					desc = "Symbols (Trouble)",
+				},
+				{
+					"<leader>cl",
+					"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+					desc = "LSP Definitions / references / ... (Trouble)",
+				},
+				{
+					"<leader>xL",
+					"<cmd>Trouble loclist toggle<cr>",
+					desc = "Location List (Trouble)",
+				},
+				{
+					"<leader>xQ",
+					"<cmd>Trouble qflist toggle<cr>",
+					desc = "Quickfix List (Trouble)",
+				},
+			},
+		},
 		-- add keymaps to show / hide type shit
 		{
 			"nvim-neo-tree/neo-tree.nvim",
@@ -991,9 +1048,31 @@ require("lazy").setup({
 				"MunifTanjim/nui.nvim",
 				"nvim-tree/nvim-web-devicons", -- optional, but recommended
 			},
+
+			-- vim.keymap.set("n", "<leader>eo", ":write<CR>", { desc = "Write" }),
 			lazy = false, -- neo-tree will lazily load itself
 		},
-		--end of idk
+		{
+			"neovim/nvim-lspconfig",
+			dependencies = {
+				{
+					"folke/lazydev.nvim",
+					ft = "lua", -- only load on lua files
+					opts = {
+						library = {
+							-- See the configuration section for more details
+							-- Load luvit types when the `vim.uv` word is found
+							{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+						},
+					},
+				},
+			},
+			config = function()
+				require("lspconfig").lua_ls.setup({})
+				require("lspconfig").texlab.setup({})
+				require("lspconfig").pyright.setup({})
+			end,
+		},
 
 		-- end of pplugin thingy
 	},
@@ -1019,3 +1098,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+-- adding diagnostic signs
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "󰌵",
+		},
+	},
+})
+
+-- LSP thingies ??
